a4d4c9dbb06fde627cf8fdb7af0b5adaf9dad884
Benjakusa <benjakusa@gmail.com>
2026-01-20 05:22:08 +0300

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,37 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    # Sentinel for "not provided"
+    _NOT_PROVIDED = object()
+    
+    def __init__(self, name=_NOT_PROVIDED, job=_NOT_PROVIDED):
+        self._name = None
+        self._job = None
+        
+        if name is not Person._NOT_PROVIDED:
+            self.name = name
+        
+        if job is not Person._NOT_PROVIDED:
+            self.job = job
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    name = property(get_name, set_name)
+    
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+    
+    job = property(get_job, set_job)
